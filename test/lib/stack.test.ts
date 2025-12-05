774c12a0965e7ec2c635f11840502f2fbb252656
Éimear McCallion <emccallion97@gmail.com>
2025-12-05 23:06:12 +0000

    Replace Jest with built-in node test framework
    Remove all jest packages and config, set up initial stack test using node:test

--- test/lib/stack.test.ts
+++ test/lib/stack.test.ts
@@ -1,25 +1,21 @@
-import { App, Stack } from 'aws-cdk-lib';
+import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { MineCloud } from '../../lib/stack';
 import { STACK_NAME } from '../../minecloud_configs/config';
-import { AmazonLinuxGeneration, AmazonLinuxImage, MachineImage } from 'aws-cdk-lib/aws-ec2';
-
+import { AmazonLinuxGeneration, AmazonLinuxImage } from 'aws-cdk-lib/aws-ec2';
+import { describe, test } from 'node:test';
 
 describe('stack', () => {
-    // test('Should contain EC2 spot instance', () => {
-    //     const app = new App();
-    //     const stack = new MineCloud(app, STACK_NAME);
-    //     const template = Template.fromStack(stack);
+    test('Should contain EC2 spot instance', () => {
+        const app = new App();
+        const stack = new MineCloud(app, STACK_NAME);
+        const template = Template.fromStack(stack);
 
-    //     template.hasResourceProperties('AWS::EC2::instance', {
-    //         machineImage: new AmazonLinuxImage({
-    //             generation: AmazonLinuxGeneration.AMAZON_LINUX_2023,
-    //             cachedInContext: true
-    //         })
-    //     });
-    // });
-
-    test('Should pass', () => {
-        expect(true).toBe(true);
+        template.hasResourceProperties('AWS::EC2::instance', {
+            machineImage: new AmazonLinuxImage({
+                generation: AmazonLinuxGeneration.AMAZON_LINUX_2023,
+                cachedInContext: true
+            })
+        });
     });
 });
